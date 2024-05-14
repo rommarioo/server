5e3d846ae4a132655674caa2d3976229da26ff5d
Roman Larionov <rommalarionov00@gmail.com>
2024-05-14 19:52:31 +0800

    test

--- server.js
+++ server.js
@@ -43,10 +43,10 @@ async function postData(url = "", formdata) {
     console.log(error);
   }
 }
-app.get("/ser", (req, res) => {
+app.get("/", (req, res) => {
   res.send("Hello World!");
 });
-app.post("/ser", async (req, res) => {
+app.post("/", async (req, res) => {
   console.log(req.get("origin"));
   if (req.body === undefined) {
     res.send("error");
